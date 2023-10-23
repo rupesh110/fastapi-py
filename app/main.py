da475ac7113fbe5109405aad94339fa69e89eb98
Rupesh <shrestharupesh110@gmail.com>
2023-10-24 00:24:40 +1100

    added new route

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World again!!!!!!!"}s
+    return {"message": "Hello World again !****!"}
 
 
 @app.get("/hello")
