4e5377f984b22f0976a22d81d97f778a7647bd4b
Rupesh <shrestharupesh110@gmail.com>
2023-10-27 23:36:38 +1100

    pushed to heroku

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World Successfully Deployed! CI/CD pipeline is working"}
 
 
 @app.get("/hello")
